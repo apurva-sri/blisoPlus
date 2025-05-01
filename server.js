752a9ad7a8840210f6199b8aa57c025afb874400
Apurva Srivastava <apurvasrivastava2403@gmail.com>
2025-05-02 00:14:20 +0530

    Day-3

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("./config/db");
-
+const engine = require('ejs-mate');
 const PORT = process.env.PORT || 5000;
+
+app.engine('ejs', engine);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use(express.json());
